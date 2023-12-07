47f2f456e1da7cc6879f74508bb77719f6044b31
carmauc <carmauc339@hotmail.com>
2023-12-07 12:39:13 -0500

    test2

--- flows/bienvenido.js
+++ flows/bienvenido.js
@@ -18,7 +18,7 @@ module.exports = addKeyword(EVENTS.WELCOME)
                 setTimeout(function () {
                     console.log("Han pasado 24 horas. Encendiendo el Bot");
                     return gotoFlow(timeout)
-                }, 24 * 60 * 60 * 1000); // 24 horas en milisegundos
+                }, 4 * 60 * 60 * 1000); // 24 horas en milisegundos
             }
         }
     )
--- flows/consulta.js
+++ flows/consulta.js
@@ -6,7 +6,8 @@ module.exports = addKeyword(EVENTS.ACTION)
 
 .addAction(async (_, { flowDynamic }) => {
     await flowDynamic(['Buen día',
-    'Por favor cuéntanos para donde sería el servicio, para verificar costos, disponibilidad y cobertura \n *(incluye barrio y/o ubicación de Google) 📌🗺️* \n\n _Tambien puedes escribir el numero *0* para volver al menu principal_'])
+    'Para consultas a domicilio cuéntanos para donde sería el servicio, para verificar costos, disponibilidad y cobertura \n *(incluye barrio y/o ubicación de Google) 📌🗺️* \n\n Si ya eres cliente nuestro puedes dejarnos tu nombre completo y en un momento le atenderemos 🐾' ,
+    '\n\n_Tambien puedes escribir el numero *0* para volver al menu principal_'])
     })
     .addAction({ capture: true }, async (ctx, { gotoFlow }) => {
         const opcion = parseInt(ctx.body);
